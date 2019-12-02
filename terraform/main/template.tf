cc313aa5d1e753350d798d9db31ddb5ec8276e52
sunil-letznav <sunil.shegu@letznav.com>
2019-12-02 11:20:41 +0530

    Adds EKS with VPC

--- terraform/main/template.tf
+++ terraform/main/template.tf
@@ -2,17 +2,25 @@ provider "aws" {
   region = "${var.region}"
 }
 
-# module "VPC" {
-#   source = "../vpc"
+# Create a VPC
+module "vpc" {
+  source = "../modules/vpc"
 
-#   region = "${var.region}"
-#   env    = "${var.env}"
-# }
-
-module "fh-es" {
-  source = "./../modules/firehose-es"
-
-  env    = "${var.env}"
   region = "${var.region}"
+  env    = "${var.env}"
 }
 
+# Create Firehose and ES
+# module "fh-es" {
+#   source = "./../modules/firehose-es"
+
+#   env    = "${var.env}"
+#   region = "${var.region}"
+# }
+
+module "eks" {
+  source = "./../modules/eks"
+
+  env = "development"
+  subnet_ids = ["${module.vpc.eks_public_1_sn_id}", "${module.vpc.eks_public_2_sn_id}", "${module.vpc.eks_public_3_sn_id}"]
+}
--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -1,3 +1,15 @@
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
+
+output "eks_public_1_sn_id" {
+  value = "${aws_subnet.eks_public_1_sn.id}"
+}
+
+output "eks_public_2_sn_id" {
+  value = "${aws_subnet.eks_public_2_sn.id}"
+}
+
+output "eks_public_3_sn_id" {
+  value = "${aws_subnet.eks_public_3_sn.id}"
+}
