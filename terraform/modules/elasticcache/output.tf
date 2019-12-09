e581b66e9f1cb9c848e11e4f884af1e08281c5df
venkatesh-apty <venkatesh.tammali@apty.io>
2019-12-09 18:43:25 +0530

    Adds EC terraform template

---
+++ terraform/modules/elasticcache/output.tf
@@ -0,0 +1,4 @@
+
+output "ec_id" {
+  value = "${aws_elasticache_cluster.ec.cluster_id}"
+}
---
+++ terraform/modules/elasticcache/variable.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  type        = "string"
+  default     = "us-east-1"
+}
+
+variable "env" {
+  description = "Environment for terraform"
+  type        = "string"
+}
+
+variable "vpc_id" {
+  type        = "string"
+}
+
+variable "subnet_ids" {
+  type = list(string)
+}
