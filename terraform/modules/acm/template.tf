a7ad1e686ba3c7abfcd1f98c49447ba0785aee89
venkatesh-apty <venkatesh.tammali@apty.io>
2019-12-27 13:46:23 +0530

    reviewchanges

--- terraform/modules/acm/template.tf
+++ terraform/modules/acm/template.tf
@@ -2,9 +2,8 @@ resource "aws_acm_certificate" "acm" {
   domain_name       = "${var.domain}" #A domain name for which the certificate should be issued
   validation_method = "DNS"
 
-  tags = "${merge(var.default_tags, map("Name", "${var.domain}", ))}"
-
   lifecycle {
     create_before_destroy = true
   }
+  tags = "${var.default_tags}"
 }
